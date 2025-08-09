c6da471420c75e90cf1f25f4a07087630163a928
RocketMenace <grizzlygit777@gmail.com>
2025-08-09 22:29:20 +0300

    build: Implementing binary search first occurrence iterative approach.

--- SearchAlgorithms/binary-search.py
+++ SearchAlgorithms/binary-search.py
@@ -1,3 +1,6 @@
+from typing import Iterable
+
+
 def binary_search(arr: list[int], target: int) -> int:
     arr = sorted(arr)
     start = 0
@@ -24,3 +27,35 @@ def binary_search_recursive(arr: list[int], target: int, start: int, end: int) -
     if guess > target:
         return binary_search_recursive(arr, target, start=start, end=mid - 1)
     return binary_search_recursive(arr, target, start=mid + 1, end=end)
+
+
+def binary_search_first_occurrence_recursive(
+    arr: list[int], target: int, start: int, end: int
+) -> int:
+    # Wrong solution, some upgrades required
+    if start > end:
+        return -1
+    mid = int((start + end) / 2)
+    if arr[mid] < target:
+        return binary_search_first_occurrence_recursive(arr, target, mid + 1, end)
+    if arr[mid] > target:
+        return binary_search_first_occurrence_recursive(arr, target, start, mid - 1)
+    return min(
+        mid, binary_search_first_occurrence_recursive(arr, target, start, mid - 1)
+    )
+
+
+def binary_search_first_occurrence(arr: list[int], target: int) -> int:
+    start = 0
+    end = len(arr) - 1
+    result = -1
+    while start <= end:
+        mid = (start + end) // 2
+        if arr[mid] == target:
+            result = mid
+            end = mid - 1
+        if arr[mid] > target:
+            end = mid - 1
+        if arr[mid] < target:
+            start = mid + 1
+    return result
