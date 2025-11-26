687c6bb4e8e4faac6e3afd20e0789e1d9c41772a
RocketMenace <grizzlygit777@gmail.com>
2025-11-26 17:58:04 +0300

    feat: Solved

--- Arrays/single_number.py
+++ Arrays/single_number.py
@@ -1,13 +1,17 @@
 
 
 def single_number(nums: list[int]) -> int | None:
+    from collections import defaultdict
+    res = defaultdict(int)
     for i in range(len(nums)):
-        if nums[i] not in nums[i + 1:] and nums[i] not in nums[:i]:
+        res[nums[i]] += 1
+    for i in range(len(nums)):
+        if res[nums[i]] == 1:
             return nums[i]
     return None
 
 
 
 if __name__ == "__main__":
-    print(single_number(nums = [2,2,1]))
+    # print(single_number(nums = [2,2,1]))
     print(single_number(nums = [4,1,2,1,2]))
