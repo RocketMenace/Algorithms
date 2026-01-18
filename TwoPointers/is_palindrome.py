5b30d073c0423c97990e9216334fa176ef5453cd
RocketMenace <grizzlygit777@gmail.com>
2026-01-18 12:13:18 +0300

    Solved

--- TwoPointers/is_palindrome.py
+++ TwoPointers/is_palindrome.py
@@ -11,8 +11,21 @@ def is_palindrome_two_pointers(x: int) -> bool:
         end -= 1
     return True
 
+def is_palindrome(x: int) -> bool:
+    if x < 0:
+        return False
+    temp = x
+    r = 0
+    while temp > 0:
+        last_digit = temp % 10
+        r = r * 10 + last_digit
+        temp //= 10
+    if r == x:
+        return True
+    return False
+
 
 
 if __name__ == "__main__":
-    print(is_palindrome_two_pointers(x = 121))
-    print(is_palindrome_two_pointers(x = -121))
+    print(is_palindrome(x = 121))
+    print(is_palindrome(x = -121))
